647716d450bb2da3ebdd92a8dfd7df900e78cf4b
K0ron <kevin.caron.91@gmail.com>
2025-03-23 00:20:05 +0100

    add header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { PrestationComponent } from './prestation/ui/prestation.component';
+import { CarteComponent } from './carte/ui/carte.component';
+import { EquipeComponent } from './equipe/ui/equipe.component';
+import { DevisComponent } from './devis/ui/devis.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'home', component: HomeComponent},
+  { path: 'prestation', component: PrestationComponent},
+  { path: 'lacarte', component: CarteComponent},
+  { path: 'lequipe', component: EquipeComponent},
+  { path: 'devis', component: DevisComponent},
+  { path: '', redirectTo: 'home', pathMatch: 'full'}
+];
