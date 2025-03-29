9adb0574ec3d47c2f9827cca5fa7f8ed9a975c00
K0ron <kevin.caron.91@gmail.com>
2025-03-29 16:56:24 +0100

    add team page

---
+++ src/app/equipe/ui/component/team-card/team-card.component.ts
@@ -0,0 +1,45 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-team-card',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './team-card.component.html',
+  styleUrl: './team-card.component.scss'
+})
+export class TeamCardComponent {
+
+  teammates: any[] = [
+
+  {
+    id: 1,
+    img: 'team/Jo.png',
+    name: 'Johann',
+    nickname: '(JO)',
+    role: 'Co-Gérant',
+    description: 'Le sérieux du groupe… enfin, jusqu’à ce qu’il lâche une blague qui fait hésiter entre rire et perplexité.',
+    devise: '"Rester pro, mais jamais trop !"'
+  },
+  {
+    id: 2,
+    img: 'team/Hadri.png',
+    name: 'Hadrien',
+    nickname: '(Ramich)',
+    role: 'Co-Gérant',
+    description: 'Grand maître du bavardage, il pourrait commenter un film en temps réel… même s&apos;il ne l&apos;a jamais vu.',
+    devise: '"Une phrase de plus, ça ne mange pas de pain !"'
+  },
+  {
+    id: 3,
+    img: 'team/Quent.png',
+    name: 'Quentin',
+    role: '',
+    nickname: '(quent)',
+    description: 'Cuisinier, pâtissier et garant des plaisirs gourmands de l’équipe. Si vous le cherchez, il est sûrement en train de goûter "juste pour vérifier".',
+    devise: '"L’amour, c’est comme le beurre : y’en a jamais trop !"'
+  },
+
+  ];
+
+}
