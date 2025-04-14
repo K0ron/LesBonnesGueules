210ca78e21e10ada121b640b88867e28ddd0cb9a
K0ron <kevin.caron.91@gmail.com>
2025-04-14 15:26:28 +0200

    add component and start search menu

---
+++ src/app/carte/ui/components/fromage-dessert-card/fromage-dessert-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fromage-dessert-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './fromage-dessert-card.component.html',
+  styleUrl: './fromage-dessert-card.component.scss'
+})
+export class FromageDessertCardComponent {
+
+}
---
+++ src/app/carte/ui/components/menu-enfant-card/menu-enfant-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-enfant-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './menu-enfant-card.component.html',
+  styleUrl: './menu-enfant-card.component.scss'
+})
+export class MenuEnfantCardComponent {
+
+}
