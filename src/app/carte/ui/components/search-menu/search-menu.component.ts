64898b1d7a42650f107694fd46188a75868bcd2d
K0ron <kevin.caron.91@gmail.com>
2025-04-14 16:53:24 +0200

    add finish header menu

--- src/app/carte/ui/components/search-menu/search-menu.component.ts
+++ src/app/carte/ui/components/search-menu/search-menu.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-search-menu',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './search-menu.component.html',
-  styleUrl: './search-menu.component.scss'
+  styleUrl: './search-menu.component.scss',
 })
-export class SearchMenuComponent {
+export class SearchMenuComponent implements OnInit {
+  @Output() categorySelected = new EventEmitter<string>();
+  selectedIndex: number | null = null;
 
+  cartes: string[] = [
+    'APERO',
+    'PLATS',
+    'ACCOMPAGNEMENTS',
+    'FROMAGE & DESSERT',
+    'BOISSONS',
+    'MENU ENFANT',
+  ];
+
+  ngOnInit(): void {
+    this.selectedIndex = 0;
+  }
+
+  selectedMenu(index: number) {
+    this.selectedIndex = index;
+    this.categorySelected.emit(this.cartes[index]);
+  }
 }
