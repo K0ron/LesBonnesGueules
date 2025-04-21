010da33c812110b72282c8e08c60cb53922989db
K0ron <kevin.caron.91@gmail.com>
2025-04-21 13:35:35 +0200

    add mobile footer

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 import { DividerModule } from 'primeng/divider';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [DividerModule],
+  imports: [DividerModule, CommonModule],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.scss'
+  styleUrl: './footer.component.scss',
 })
 export class FooterComponent {
+  openSection: string | null = null;
 
+  toggleSection(section: string) {
+    this.openSection = this.openSection === section ? null : section;
+  }
+
+  isOpen(section: string): boolean {
+    return this.openSection === section;
+  }
 }
