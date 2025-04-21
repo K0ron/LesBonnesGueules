5699a784f09adbe838d5479d4b07ab697fcd9024
K0ron <kevin.caron.91@gmail.com>
2025-04-21 16:16:45 +0200

    add mibile equipe

--- src/app/equipe/ui/component/team-card/team-card.component.ts
+++ src/app/equipe/ui/component/team-card/team-card.component.ts
@@ -6,40 +6,39 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [CommonModule],
   templateUrl: './team-card.component.html',
-  styleUrl: './team-card.component.scss'
+  styleUrl: './team-card.component.scss',
 })
 export class TeamCardComponent {
-
   teammates: any[] = [
-
-  {
-    id: 1,
-    img: 'team/Jo.png',
-    name: 'Johann',
-    nickname: '(JO)',
-    role: 'Co-Gérant',
-    description: 'Le sérieux du groupe… enfin, jusqu’à ce qu’il lâche une blague qui fait hésiter entre rire et perplexité.',
-    devise: '"Rester pro, mais jamais trop !"'
-  },
-  {
-    id: 2,
-    img: 'team/Hadri.png',
-    name: 'Hadrien',
-    nickname: '(Ramich)',
-    role: 'Co-Gérant',
-    description: 'Grand maître du bavardage, il pourrait commenter un film en temps réel… même s&apos;il ne l&apos;a jamais vu.',
-    devise: '"Une phrase de plus, ça ne mange pas de pain !"'
-  },
-  {
-    id: 3,
-    img: 'team/Quent.png',
-    name: 'Quentin',
-    role: '',
-    nickname: '(quent)',
-    description: 'Cuisinier, pâtissier et garant des plaisirs gourmands de l’équipe. Si vous le cherchez, il est sûrement en train de goûter "juste pour vérifier".',
-    devise: '"L’amour, c’est comme le beurre : y’en a jamais trop !"'
-  },
-
+    {
+      id: 1,
+      img: 'team/Jo.png',
+      name: 'Johann',
+      nickname: '(JO)',
+      role: 'Co-Gérant',
+      description:
+        'Le sérieux du groupe… enfin, jusqu’à ce qu’il lâche une blague qui fait hésiter entre rire et perplexité.',
+      devise: '"Rester pro, mais jamais trop !"',
+    },
+    {
+      id: 2,
+      img: 'team/Hadri.png',
+      name: 'Hadrien',
+      nickname: '(Ramich)',
+      role: 'Co-Gérant',
+      description:
+        "@Grand maître du bavardage, il pourrait commenter un film en temps réel… même s'il ne l'a jamais vu.",
+      devise: '"Une phrase de plus, ça ne mange pas de pain !"',
+    },
+    {
+      id: 3,
+      img: 'team/Quent.png',
+      name: 'Quentin',
+      role: '',
+      nickname: '(quent)',
+      description:
+        'Cuisinier, pâtissier et garant des plaisirs gourmands de l’équipe. Si vous le cherchez, il est sûrement en train de goûter "juste pour vérifier".',
+      devise: '"L’amour, c’est comme le beurre : y’en a jamais trop !"',
+    },
   ];
-
 }
